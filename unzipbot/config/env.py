900cf474042af05a4eb0f4344625ce3ddf4ba65c
EDM115 <dev@edm115.eu.org>
2025-04-21 04:49:35 +0200

    feat(config): bases for more customization

--- unzipbot/config/env.py
+++ unzipbot/config/env.py
@@ -1,3 +1,4 @@
+from importlib.metadata import PackageMetadata, metadata, version
 from os import environ
 from pathlib import Path
 
@@ -6,14 +7,20 @@ PACKAGE_ROOT: Path = Path(__file__).resolve().parents[1]
 
 
 class Env:
-    APP_ID = environ.get("APP_ID")
-    API_HASH = environ.get("API_HASH")
-    BASE_LANGUAGE = environ.get("BASE_LANGUAGE")
-    BOT_TOKEN = environ.get("BOT_TOKEN")
-    BOT_OWNER = environ.get("BOT_OWNER")
-    DYNO = environ.get("DYNO")
-    LOGS_CHANNEL = environ.get("LOGS_CHANNEL")
-    MONGODB_DBNAME = environ.get("MONGODB_DBNAME")
-    MONGODB_URL = environ.get("MONGODB_URL")
-    PACKAGE_NAME = pkg_name
+    APP_ID: str | None = environ.get("APP_ID")
+    API_HASH: str | None = environ.get("API_HASH")
+    BASE_LANGUAGE: str | None = environ.get("BASE_LANGUAGE")
+    BOT_TOKEN: str | None = environ.get("BOT_TOKEN")
+    BOT_OWNER: str | None = environ.get("BOT_OWNER")
+    DYNO: str | None = environ.get("DYNO")
+    FRAMEWORK_METADATA: PackageMetadata = metadata("pyrofork")
+    LOGS_CHANNEL: str | None = environ.get("LOGS_CHANNEL")
+    MONGODB_DBNAME: str | None = environ.get("MONGODB_DBNAME")
+    MONGODB_URL: str | None = environ.get("MONGODB_URL")
+    PACKAGE_NAME: str = pkg_name
     ROOT_DIR = str(PACKAGE_ROOT)
+
+    class Versions:
+        PYROFORK: str = version("pyrofork")
+        PYTHON = "3.12.10"  # TODO : grab this from terminal once cli is done
+        UNZIPBOT = "7.3.0"
